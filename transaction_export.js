405eee9ace57914a4250cbe466db1d255a79f285
R Game <3510898+rgamec@users.noreply.github.com>
2019-03-30 17:00:41 +0000

    Refactoring Code
    Now using a temporary data structure to store found transactions

--- transaction_export.js
+++ transaction_export.js
@@ -6,12 +6,30 @@ var script = document.createElement('script');
 script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(script);
 
+/* Storing transactions in this data structure */
+var transactions = [];
+
 /* Due to encryptQuery, we'll iterate over the transactions by searching the URLs */
 $('a[href*="/transactions/"]').parent()
                               .parent()
                               .each(function(){
-                                var transaction = $( this ).find(":first-child > span:nth-child(2)").context.outerText;
-                                console.log(transaction);
+                                var transaction = {};
+  
+                                /* Fetching transaction description */
+                                transaction.description = $(this).find("div:eq(2)").get()[0].innerText.split("\n")[0];
+  
+                                /* Fetching transaction amount */
+                                transaction.amount = $(this).find("div:eq(3)").get()[0].innerText;
+  
+                                /* Fetching transaction date */  
+                                /* Remove the split() for the full datetime */
+                                transaction.date = $(this).find("div:eq(2) time").get()[0].dateTime.split("T")[0];
+
+                                /* console.log(transaction);*/
+                                transactions.push(transaction);
                               });
                               
 /* TODO: Add a 'Copy' button to the site's DOM to send the CSV text straight to the clipboard */
+transactions.forEach(function(transaction){
+    console.log(transaction.date + "," + transaction.description + "," + transaction.amount);
+});
